49f2208dfa3d918615428bb1af87c515a9c2e0d9
Liang Bo Wang <ccwang002@gmail.com>
2013-09-03 11:13:14 +0800

    delete trailing whitespace

--- src/step_function.class.cpp
+++ src/step_function.class.cpp
@@ -8,18 +8,18 @@ typedef std::vector<double> NumVec;
 struct StepFunction {
 	NumVec x;
 	NumVec y;
-	
-	StepFunction(NumericVector Rx, NumericVector Ry) 
-	: x(as<NumVec>(Rx)), y(as<NumVec>(Ry)) { 
+
+	StepFunction(NumericVector Rx, NumericVector Ry)
+	: x(as<NumVec>(Rx)), y(as<NumVec>(Ry)) {
 		if (x.size() + 1 != y.size()) throw std::invalid_argument("length(Rx) + 1 == length(Ry) does not hold");
 	}
 	~StepFunction() { }
-	
+
 	double call(double input) const {
 		NumVec::const_iterator i = std::upper_bound(x.begin(), x.end(), input);
 		return y[i - x.begin()];
 	}
-	
+
 	SEXP sort_call(NumericVector input) {
 		NumVec::const_iterator i = x.begin();
 		NumericVector retval(input.size());
@@ -29,7 +29,7 @@ struct StepFunction {
 		}
 		return retval;
 	}
-	
+
 private:
 	StepFunction(const StepFunction&);
 	void operator=(const StepFunction&);
@@ -38,7 +38,7 @@ private:
 //double step_integrate
 
 RCPP_MODULE(recurrentR) {
-	
+
 	class_<StepFunction>("StepFunction")
 	.constructor<NumericVector, NumericVector>()
 	.field_readonly("x", &StepFunction::x)
@@ -46,7 +46,7 @@ RCPP_MODULE(recurrentR) {
 	.method("call", &StepFunction::call)
 	.method("sort_call", &StepFunction::sort_call)
 	;
-	
+
 }
 
 template<class T>
